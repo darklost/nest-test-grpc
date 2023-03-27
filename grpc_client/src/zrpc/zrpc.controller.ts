b4d0b04f39ef63a377d2c9948890e8749eb9de29
darklost.me <darklost.me@qq.com>
2023-03-27 20:58:26 +0800

    实现动态创建服务

--- grpc_client/src/zrpc/zrpc.controller.ts
+++ grpc_client/src/zrpc/zrpc.controller.ts
@@ -2,10 +2,14 @@ import { Metadata, ServerDuplexStream } from '@grpc/grpc-js';
 import { Controller, Get, Inject, OnModuleInit, Param } from '@nestjs/common';
 import {
   ClientGrpc,
+  ClientProxyFactory,
   GrpcMethod,
+  GrpcOptions,
   GrpcStreamCall,
   GrpcStreamMethod,
+  Transport,
 } from '@nestjs/microservices';
+import { join } from 'path';
 import { Observable, ReplaySubject, Subject } from 'rxjs';
 import { toArray } from 'rxjs/operators';
 import { ZMessagePack, ZrpcService } from './zrpc.interface';
@@ -16,7 +20,7 @@ export class ZrpcController implements OnModuleInit {
   private traceId: number = 0;
   private zrpcService: ZrpcService;
   private zrpcRepSub$: ReplaySubject<ZMessagePack>;
-  constructor(@Inject('ZRPC_PACKAGE') private readonly client: ClientGrpc) { }
+  // constructor(@Inject('ZRPC_PACKAGE') private readonly client: ClientGrpc) { }
 
   sendZrpcReq(data: string): boolean {
     if (this.zrpcRepSub$ == null || this.zrpcRepSub$ == undefined) {
@@ -32,12 +36,24 @@ export class ZrpcController implements OnModuleInit {
   }
   onModuleInit() {
     console.log("加载ZrpcController onModuleInit");
-    this.zrpcService = this.client.getService<ZrpcService>('ZrpcService');
 
+    // this.zrpcService = this.client.getService<ZrpcService>('ZrpcService');
+    this.zrpcService = ClientProxyFactory.create({
+      transport: Transport.GRPC,
+      options: {
+        url: "0.0.0.0:3000",
+        package: 'zrpc',
+        protoPath: join(__dirname, '../../../proto/zrpc.proto'),
+      },
+    }).getService<ZrpcService>('ZrpcService');
+
+
+    // 根据grpcServiceConfigs创建多个grpc服务
+    // const grpcServices = grpcServiceConfigs.map(grpcServiceConfig => {
+    //   return ClientProxyFactory.create(grpcServiceConfig);
+    // });
 
     this.zrpcRepSub$ = new ReplaySubject<ZMessagePack>();
-
-
 
 
 
@@ -45,7 +61,7 @@ export class ZrpcController implements OnModuleInit {
     stream.subscribe({
       next: (resData) => {
         if (resData.data) {
-          console.log(`next`, resData);
+          console.log(`zrpc next`, resData);
         }
       },
       error: (err) => {
@@ -54,6 +70,9 @@ export class ZrpcController implements OnModuleInit {
         this.zrpcRepSub$.complete();
         this.zrpcRepSub$.unsubscribe();
         this.zrpcRepSub$ = null;
+      },
+      complete: () => {
+        console.log(`zrpc complete`);
       },
     })
   }
--- grpc_client/src/zrpc/zrpc.module.ts
+++ grpc_client/src/zrpc/zrpc.module.ts
@@ -8,21 +8,21 @@ import { ZrpcController } from './zrpc.controller';
   imports: [
 
     ConfigModule,
-    ClientsModule.registerAsync([
-      {
-        imports: [ConfigModule],
-        inject: [ConfigService],
-        name: 'ZRPC_PACKAGE',
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.GRPC,
-          options: {
-            url: "0.0.0.0:3000",
-            package: 'zrpc',
-            protoPath: join(__dirname, '../../../proto/zrpc.proto'),
-          },
-        }),
-      },
-    ]),
+    // ClientsModule.registerAsync([
+    //   {
+    //     imports: [ConfigModule],
+    //     inject: [ConfigService],
+    //     name: 'ZRPC_PACKAGE',
+    //     useFactory: (configService: ConfigService) => ({
+    //       transport: Transport.GRPC,
+    //       options: {
+    //         url: "0.0.0.0:3000",
+    //         package: 'zrpc',
+    //         protoPath: join(__dirname, '../../../proto/zrpc.proto'),
+    //       },
+    //     }),
+    //   },
+    // ]),
 
   ],
   controllers: [ZrpcController],
